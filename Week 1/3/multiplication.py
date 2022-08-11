9da39aac58284d6acb9ee2aca0fc5d995d4d9fd8
sara-abidi <saraabidi428@gmail.com>
2022-08-11 23:23:25 +0100

    Completed 11/08/22

---
+++ Week 1/3/multiplication.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    # Enter your solution here
+    number = 4
+    for i in range(11):
+        print(f"{number} multiplied by {i} is {number*i}")
+if __name__ == "__main__":
+    main()
